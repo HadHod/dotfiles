f2def18950b40db6e2325b21e95c6e6f55ec3b71
Maciej Młynarski <maciej.mlynarski@gmail.com>
2014-05-07 18:31:30 +0200

    Create SublimText - settings.js

---
+++ SublimText - settings.js
@@ -0,0 +1,12 @@
+{
+	"color_scheme": "Packages/Color Scheme - Default/Sunburst.tmTheme",
+	"draw_minimap_border": true,
+	"draw_white_space": "all",
+	"font_size": 11,
+	"highlight_line": true,
+	"line_numbers": true,
+	"remember_open_files": true,
+	"tab_size": 4,
+	"translate_tabs_to_spaces": false,
+	"trim_trailing_white_space_on_save": true
+}
